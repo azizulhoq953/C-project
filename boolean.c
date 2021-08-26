3a5fd41588459a6be1a66d58c42637db232a134f
azizul_hoq <azizulhoq4305@gamil.com>
2021-08-26 20:59:18 +0600

    C-UpdateCode

---
+++ boolean.c
@@ -0,0 +1,12 @@
+//using namespace std;
+#include<stdio.h>
+string bool_to_word(bool value);
+int main(){
+    char True
+{
+    if bool == True:
+        return "Yes"
+    else:
+        return "No"
+}
+}
