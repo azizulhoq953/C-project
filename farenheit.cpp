f2ff77dc3fe55afb534afb576d28815d2639caef
azizul_hoq <azizulhoq4305@gamil.com>
2021-10-20 10:21:37 +0600

    ansp

---
+++ farenheit.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+ int fern,cels;
+cout<<"Enter A celcious Number:";
+cin>>cels;
+
+
+fern=1.8 * cels+32;
+cout<<"Your Farenheait Value:"<<fern;
+
+/*//farenheit to celcious Convert:
+ cout<<"Enter Farenheit Number:";
+ cin>>fern;
+
+ cels=1.8 * fern+32;
+
+
+ cout<<"Your Celcias Value:"<<cels;
+*/
+getch();
+}
