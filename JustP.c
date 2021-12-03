0b9c2d83a7da543de8472b40a5a76ed339323887
azizulhoq953 <azizulhoq2041@gmail.com>
2021-12-03 21:00:39 +0600

    FunctionCall

---
+++ JustP.c
@@ -0,0 +1,45 @@
+
+#include<stdio.h>
+
+int printStudentInfo();
+float Id();
+char Session();
+int AcademyNo();
+int main(){
+
+printStudentInfo();
+
+}
+
+int printStudentInfo(){
+
+Id();
+Session();
+AcademyNo();
+printf("Almost Print Student Info\n");
+
+}
+
+float Id(){
+
+printf("ID: 12021002\n");
+
+printf("End Of Id\n");
+
+return 0;
+
+}
+
+char Session(){
+ printf("Section: C\n");
+
+ printf("\n End Of Char\n");
+ return 0;
+}
+
+int AcademyNo(){
+printf("Building No: 3\n");
+printf("End Of Academy\n\n");
+return 0;
+}
+
