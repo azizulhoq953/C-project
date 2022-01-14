7c5ac58005d40d10e8364a7ae65c6c2fc3390017
azizulhoq953 <azizulhoq2041@gmail.com>
2022-01-14 18:23:50 +0600

    cpp_H_work

---
+++ C++/cls/C_H_Work.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int n;
+float m;
+double num;
+long long Id;
+char str[10];
+string name;
+
+//int data type define
+cout<<"Enter Any Integer Numbers:";
+cin>>n;
+cout<<n<<"\n";
+
+//float data type define
+cout<<"Enter Any floating Numbers:";
+cin>>m;
+cout<<m<<"\n";
+
+//double data type define
+cout<<"Enter Any double data type Numbers:";
+cin>>num;
+cout<<num<<"\n";
+
+//long long data type define
+cout<<"Enter your Student ID:";
+cin>>Id;
+cout<<Id<<"\n";
+
+//string data type define
+cout<<"Enter Your Full Name:";
+cin>>name;
+cout<<name<<"\t";
+
+//all data
+cout<<"\n\nAll information:"<<"\n";
+
+cout<<n<<"\n";
+cout<<m<<"\n";
+cout<<num<<"\n";
+cout<<Id<<"\n";
+cout<<name<<"\n";
+}
