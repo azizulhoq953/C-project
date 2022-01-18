89e79c45f17c02c5c196dd6f09ce4cbc7bb57a73
azizulhoq953 <azizulhoq2041@gmail.com>
2022-01-18 13:50:54 +0600

    cls-second

---
+++ C++/cls/cls2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int Id;
+double x;
+string name;
+cout<<"Enter your Id"<<"\n"<<endl;
+
+cin>>Id;
+
+cout<<"Enter Your Name:"<<"\n"<<endl;
+
+cin>>name;
+
+return 0;
+
+}
