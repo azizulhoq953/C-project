001419c242c08fe97ecb827ec4d6295d81f06214
azizulhoq953 <azizulhoq2041@gmail.com>
2022-01-20 19:22:24 +0600

    M

--- C++/cls/cls2.cpp
+++ C++/cls/cls2.cpp
@@ -1,7 +1,35 @@
 #include<iostream>
 using namespace std;
 
+
 int main(){
+
+int a, b, sum;
+wchar_t c;
+
+cout<<"Enter Any First Numbers:"<<"\n";
+
+cin>>a;
+ //cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+
+ c="az";
+
+cout<<c;
+
+cout<<"Enter Your Any Second Number:";
+
+cin>>b;
+
+sum=a+b;
+
+cout<<"\t"<<"Sum Of="<<sum<<"\n";
+
+return 0;
+}
+
+
+
+/*int main(){
 
 int Id;
 double x;
@@ -16,4 +44,4 @@ cin>>name;
 
 return 0;
 
-}
+}*/
