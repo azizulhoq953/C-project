4b7f76e8e08d1e36c291dd8aace57da6df613fcb
azizulhoq953 <azizulhoq2041@gmail.com>
2022-02-25 02:06:33 +0000

    c&cpp_separate

--- C++/enum.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-enum colors{red=5, black};
-enum suit{heart, diamond=8,khata=1, spade=3, club,seminar};
-int main() {
-   cout <<"The value of enum color : "<<red<<","<<black;
-   cout <<"\nThe default value of enum suit : "<<heart<<","<<diamond<<","<<","<<khata<<spade<<","<<club<<","<<seminar;
-   return 0;
-}
