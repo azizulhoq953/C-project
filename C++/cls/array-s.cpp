45d7b6a79552e0d49f6e3b77858975e5d5f762f3
azizulhoq953 <azizulhoq2041@gmail.com>
2022-02-26 07:03:28 +0000

    cls-s-p

--- C++/cls/array-s.cpp
+++ C++/cls/array-s.cpp
@@ -1,3 +1,26 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+// int main() {
+//    int *array, size;
+//   //  array= new int [size];
+//    cout<<"Enter size of array: "<<endl;
+//    cin>>size;
+//    array = new int [size];
+//    cout<<"Enter array elements: "<<endl;
+
+//    for (int i = 0; i < size; i++)
+//    cin>>array[i];
+//    cout<<"The array elements are: ";
+
+//    for(int i = 0; i < size; i++)
+//    cout<<array[i]<<" ";
+//    cout<<endl;
+//   cout<<"Memory Address of:"<< &array<<"\n";
+//    return 0;
+// }
+
 
 #include <iostream>
 using namespace std;
@@ -5,14 +28,23 @@ using namespace std;
 
 int main ()
 {
+int n;
 
-int Array [20] = {16, 2, 77, 40, 30,40,60,22,34,36,38,11,12,34,56,78,16,17,18,19};
-for(int i=0; i<20; i++)
-  cout << Array[i] <<"\n";
+int Array[20]= {16, 2, 77, 40, 30,40,60,22,34,36,38,11,12,34,56,78,16,17,18,19};
+// for(int i=0; i<n; ++i){
+  
+// }
+
+
+for (int i=0; i<20; ++i){
+  cout<<Array[i]<<" "<<endl;
+}
+ 
   return 0;
 
 
-/* //string type
+/*
+//string type
 
 string Cars [20] = {"Toyoto","Lambergini","Marcedes","Organizations:","XYZ"};
 for(int i=0; i<5; i++)
