966447dce14519820965c93013b6e30fa2a29203
azizulhoq953 <azizulhoq2041@gmail.com>
2022-03-18 15:25:29 +0600

    solve-p

--- problem solving/URI/sum.c
+++ problem solving/URI/sum.c
@@ -1,9 +1,23 @@
+/*
+//first problem
 #include<stdio.h>
 
 int main(){
 int A,B,SOMA;
 scanf("%d %d",&A,&B);
-SOMA=A+B;
+PROD=A+B;
 printf("SOMA = %d\n",SOMA);
+return 0;
+}
+*/
+
+//second problem
+#include<stdio.h>
+
+int main(){
+int A,B,PROD;
+scanf("%d %d",&A,&B);
+PROD=A*B;
+printf("PROD = %d\n",PROD);
 return 0;
 }
