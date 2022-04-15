4c5444299db9bdf059d8e1563eea841ba769ed67
azizulhoq953 <azizulhoq2041@gmail.com>
2022-04-15 22:48:54 +0600

    binary-sort&search

---
+++ C++/R-practice/storage.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+/*
+//extern
+void show();
+
+int x=26;
+
+int main(){
+
+//int x=100;
+extern int x; //it's external variable assign
+x=100;
+cout<<x<<endl;
+show();
+
+
+
+}
+
+void show(){
+
+cout<<x<<endl;
+
+}*/
+
+int main(){
+
+register int i;
+
+for(i=0;i<100;i++)
+cout<<i<<endl;
+
+}
+
