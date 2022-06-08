b1b08338cfe794061fd4b56a65abc36083cd6cd1
azizulhoq953 <azizulhoq2041@gmail.com>
2022-06-08 20:43:20 +0600

    beecrwed-average

---
+++ C++/ProblemSolving/beecwed.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     float a,b,sum;
+
+
+     cin>>a;
+     cin>>b;
+     sum=a+b;
+     float divided;
+     divided=sum/2;
+     cout<<"MEDIA = "<<divided<<endl;
+
+    return 0;
+}
