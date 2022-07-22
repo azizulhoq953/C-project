b080213a1c4a71db6b445b3170e9799d4c502f08
azizulhoq953 <azizulhoq2041@gmail.com>
2022-07-22 23:05:30 +0600

    jprtc

---
+++ C++/ProblemSolving/codechef/age limit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int t,x,y,A;
+cin>>t;
+for(int i=0;i<t; i++){
+
+    cin>>x>>y>>A;
+    if(A>=x && A<y)
+        cout<<"YES"<<endl;
+
+    else
+        cout<<"NO"<<endl;
+
+}
+
+return 0;
+}
