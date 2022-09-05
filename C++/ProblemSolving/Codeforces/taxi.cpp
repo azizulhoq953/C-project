bacc0415dba6ea3e20a5b1825d467cd388cd9617
azizulhoq953 <azizulhoq2041@gmail.com>
2022-09-05 21:09:10 +0600

    mslve

---
+++ C++/ProblemSolving/Codeforces/taxi.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,i,sum;
+    while(cin>>x){
+        long long a[x];
+        sum=0;
+        for(i=0;i<x;i++){
+            cin>>a[i];
+            sum+=a[i];
+        }
+      sum=sum/4;
+
+
+                break;
+        }
+        cout<<sum<<endl;
+
+    return 0;
+}
+
