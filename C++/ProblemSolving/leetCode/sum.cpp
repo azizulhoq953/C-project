778a85254ad9c1702a1e69787fe1dc06b217f9e5
azizulhoq953 <azizulhoq2041@gmail.com>
2022-11-16 20:33:10 +0600

    nslve

--- C++/ProblemSolving/leetCode/sum.cpp
+++ C++/ProblemSolving/leetCode/sum.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-
-        int n = nums.size();
-        vector<int> nums2 = nums;
-
-        sort(nums2.begin(), nums2.end());
-
-
-        int a, b;
-        for(int l = 0, r = n-1; l <= r; ){
-            if(nums2[l] + nums2[r] < target){
-                ++l;
-            }else if(nums2[l] + nums2[r] > target){
-                --r;
-            }else{
-                a = nums2[l];
-                b = nums2[r];
-                break;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode* dummy = new ListNode(0);
+        ListNode* l = dummy;
+        while (l1 || l2) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
             }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            if (sum > 9) {
+                carry = 1;
+                sum = sum % 10;
+            }
+            else {
+                carry = 0;
+            }
+
+            l->next = new ListNode(sum);
+            l = l->next;
         }
 
-
-        vector<int> ans(2);
-
-        for(int i = 0; i < n; ++i){
-            if(nums[i] == a){
-                ans[0] = i;
-                break;
-            }
+        if (carry) {
+            l->next = new ListNode(carry);
         }
 
-        for(int i = n-1; i >= 0; --i){
-            if(nums[i] == b){
-                ans[1] = i;
-                break;
-            }
-        }
-
-        return ans;
+        return dummy->next;
     }
 };
---
+++ C++/contest/code-samuarai/string sum.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+int n,s,m;
+
+while(n--){
+    cin>>s>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;i<m;j++){
+
+    cout<<i<<j<<endl;
+        }
+
+    }
+
+    cout<<s<<m<<endl;
+}
+
+
+}
