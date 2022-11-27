f23bd1d67d8f6fa28ea16879acf9472c35311799
azizulhoq953 <azizulhoq2041@gmail.com>
2022-11-27 21:18:31 +0600

    frhn-prt

---
+++ C++/ProblemSolving/Codeforces/subline/technical-support.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main(){
+int n;
+
+cin>>n;
+while(n--){
+char cahrr[50];
+int n;
+for(int i=0;i<n;i++){
+	cin>>charr[i];
+}
+
+
+
+}
+
+
+
+
+}
