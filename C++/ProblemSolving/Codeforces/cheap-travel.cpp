0425593f61b8711a2468def51e74666dabbaa3f0
azizulhoq953 <azizulhoq2041@gmail.com>
2023-01-15 14:16:01 +0600

    slve

---
+++ C++/ProblemSolving/Codeforces/cheap-travel.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, m, a, b;
+    int x;
+    cin>>n>>m>>a>>b;
+
+    if(m*a>b){
+         x = (n%m) * a;
+        if(x>b){
+            cout<<n/m*b + b<<endl;
+        }
+        else{
+            cout<<n/m *b + x <<endl;
+        }
+    }
+    else{
+        cout<<n*a<<endl;
+    }
+    
+    return 0;
+}
---
+++ C++/ProblemSolving/Codeforces/gameWithSticks.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int i=1;
+    int count=0;
+    for(;;){
+     if(a-i==0) break;
+     if (b-i==0) break;
+     i++;
+     count++;
+     
+    }
+    if (count%2==0)
+    {
+       cout<<"Akshat";
+    }else
+    cout<<"Malvika";
+    
+}
